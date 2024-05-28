26220b8f41f287d0c8b4f64d6e35597f21d27501
R2 <R2@tctcore.com>
2024-05-28 17:57:35 +0800

    finish

--- asset/js/page2.js
+++ asset/js/page2.js
@@ -1,3 +1,27 @@
 // aos.js
 AOS.init();
 
+
+$(document).ready(function() {
+  // $('.center-menu .row .col:nth-child(1)').hover(
+  //   function() {
+  //     $('.center-menu-mask').css('display', 'none');
+  //   },
+  //   function() {
+  //     $('.center-menu-mask').css('display', 'block');
+  //   }
+  // );
+  $('.center-menu .row .col:not(:first-child)').hover(
+    function() {
+      console.log(123)
+      $(this).children('[class^="center-menu-"]').css('display', 'block');
+      $('.center-menu-mask').css('display', 'block');
+    },
+    function() {
+      console.log(456)
+      $(this).children('[class^="center-menu-"]').css('display', 'none');
+      $('.center-menu-mask').css('display', 'none');
+    }
+  );
+
+});
