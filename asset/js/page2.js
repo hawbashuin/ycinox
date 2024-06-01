5457da6d8c2c81e31272b345644350f906e42113
R2 <R2@tctcore.com>
2024-06-01 22:30:24 +0800

    add feature

--- asset/js/page2.js
+++ asset/js/page2.js
@@ -3,14 +3,31 @@ AOS.init();
 
 
 $(document).ready(function() {
-  // $('.center-menu .row .col:nth-child(1)').hover(
-  //   function() {
-  //     $('.center-menu-mask').css('display', 'none');
-  //   },
-  //   function() {
-  //     $('.center-menu-mask').css('display', 'block');
-  //   }
-  // );
+  // 取得 back to top 按鈕
+  let backToTopBtn = document.getElementById("btn-back-to-top");
+
+  // 捲軸下滑160之後出現 back to top 按鈕
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 160 ||
+      document.documentElement.scrollTop > 160
+    ) {
+      backToTopBtn.style.display = "block";
+    } else {
+      backToTopBtn.style.display = "none";
+    }
+  }
+  // 點選按鈕後回到頁面頂端
+  backToTopBtn.addEventListener("click", backToTop);
+
+  function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
   $('.center-menu .row .col:not(:first-child)').hover(
     function() {
       $(this).children('[class^="center-menu-"]').css('display', 'block');
--- asset/js/page3.js
+++ asset/js/page3.js
@@ -22,29 +22,55 @@ $(document).ready(function() {
   }
   animateNumber()
 
-  console.log($('.owl-carousel').html())
-  var owl = $('.owl-carousel');
-  owl.owlCarousel({
-    items: 3,
-    loop: true,
-    margin: 10,
-    nav: true,
-    autoplay: true,
-    autoplayTimeout: 3000, // 每3秒自動播放下一張
-    animateIn: 'zoomIn', // 進場動畫為 zoomIn
-    animateOut: 'zoomOut' // 出場動畫為 zoomOut
-  });
-  function startZoomAnimation() {
-    console.log("123123")
-    $('.image-zoom').addClass('active');
+  // 取得 back to top 按鈕
+  let backToTopBtn = document.getElementById("btn-back-to-top");
+
+  // 捲軸下滑160之後出現 back to top 按鈕
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 160 ||
+      document.documentElement.scrollTop > 160
+    ) {
+      backToTopBtn.style.display = "block";
+    } else {
+      backToTopBtn.style.display = "none";
+    }
+  }
+  // 點選按鈕後回到頁面頂端
+  backToTopBtn.addEventListener("click", backToTop);
+
+  function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
   }
 
-  function stopZoomAnimation() {
-    $('.image-zoom').removeClass('active');
-  }
+  // console.log($('.owl-carousel').html())
+  // var owl = $('.owl-carousel');
+  // owl.owlCarousel({
+  //   items: 3,
+  //   loop: true,
+  //   margin: 10,
+  //   nav: true,
+  //   autoplay: true,
+  //   autoplayTimeout: 3000, // 每3秒自動播放下一張
+  //   animateIn: 'zoomIn', // 進場動畫為 zoomIn
+  //   animateOut: 'zoomOut' // 出場動畫為 zoomOut
+  // });
+  // function startZoomAnimation() {
+  //   console.log("123123")
+  //   $('.image-zoom').addClass('active');
+  // }
 
-  // 每 5 秒自動開始放大動畫
-  setInterval(startZoomAnimation, 1000);
+  // function stopZoomAnimation() {
+  //   $('.image-zoom').removeClass('active');
+  // }
+
+  // // 每 5 秒自動開始放大動畫
+  // setInterval(startZoomAnimation, 1000);
 
   // 動畫進行期間，停止自動播放
   // $('.image-zoom').on('transitionend', function() {
